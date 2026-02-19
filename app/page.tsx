c8939dcbeb7007e65b4fe11072150c28270ab8bf
shubhsingh13jann <shubhsingh.13jann@gmail.com>
2026-02-19 12:24:51 +0530

    SupaBase_Connectivity

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hi, My name is Shubh Singh. This is my fist commit after setup of pnpm
+      Hi, My name is Shubh Singh. This is my fist commit after setup of pnpm and next js
     </div>
   );
 }
